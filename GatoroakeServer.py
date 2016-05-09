a08c85b8e0fd45bee6f973933a6e2fe25cd0f94f
hern <ahernandez69@yahoo.com>
2016-05-08 22:32:30 -0400

    basic screens work

--- GatoroakeServer.py
+++ GatoroakeServer.py
@@ -14,9 +14,17 @@ import Image, ImageDraw, ImageFont
 import cdgPlayer
 
 path = "/var/www/songqueue/"
-cdgpath = "/home/hern/Karaoke/A-Z"
+cdgpath = "/home/hern/A-Z"
+#cdgpath = "/home/hern/Karaoke/A-Z"
 mp3path = "/home/hern/mp3"
 introSleepTime = 10
+
+
+PAUSED=1
+INTRO=2
+PLAYING=3
+WAITING=4
+LIST=5
 
 # Import the optimised C version if available, or fall back to Python
 try:
@@ -159,7 +167,6 @@ def defaultErrorPrint(ErrorString):
 def main2():
 
     songlist = []
-    waitScreen()
     while 1 == 1:
       try:
         dirlist = os.listdir(path)
@@ -241,21 +248,116 @@ class GatorokeServer():
         self.clock = pygame.time.Clock()
 
     def run(self):
+        '''
+        States in Loop:
+        PAUSED
+        INTRO
+        PLAYING
+        WAITING
+        LIST
+        '''
+        state=WAITING
+        oldstate=None
+        waitCounter = 0
+        sleepTime = 10
+        songlist = []
         while 1:
-            self.clock.tick(60)
-        #Handle Input Events
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    return
-                elif event.type == KEYDOWN and event.key == K_ESCAPE:
-                    return
-                elif event.type == KEYDOWN and event.key == K_SPACE:
-                    self.writeMessage("SPACE pressed\nKewl", clearbg=True)
-                elif event.type == KEYDOWN and event.key == K_a:
-                    self.writeMessage("a pressed", clearbg=True)
             self.screen.blit(self.background, (0, 0))
             pygame.display.flip()
+            self.clock.tick(60)
+            #Handle Input Events
+            for event in pygame.event.get():
+                self.checkQuit(event)
+                if state == LIST and event.type == KEYDOWN and event.key == K_SPACE:
+                    state = INTRO
+                elif event.type == KEYDOWN and event.key == K_a:
+                    self.writeMessage("a pressed", clearbg=True)
+                elif event.type == KEYDOWN and event.key == K_p:
+                    if state == PAUSED:
+                        state = oldstate
+                    else:
+                        oldstate = state
+                        state = PAUSED
+            if state == PAUSED:
+                self.writeMessage('Paused\nPress "P" to continue', clearbg=True)
+                continue
+            if state == WAITING:
+                requestList = self.getDirList()
+                if len(requestList) > 0:
+                    state = LIST
+                else:
+                    self.writeMessage("Checking Queue...", clearbg=True)
+                    continue
+            elif state == LIST:
+                displayString = self.formatRequestString(requestList)
+                self.writeMessage("Upcoming Singers:\n%s\n\nSPACE to continue" %displayString, clearbg = True)
+            elif state == INTRO:
+                #Intro Timer
+                if waitCounter > (sleepTime * 100):
+                    waitCounter = 0
+                    state = WAITING
+                    self.playSong(requestList)
+                    continue
+                waitCounter += 1
+                self.writeMessage("Intro Screen Countdown: %d" %(10 - (waitCounter / 100)), clearbg = True)
+                #showKaraokeIntro(requestList)
+                continue
 
+    def formatRequestString(self, requestList):
+        retString = ""
+        for i in range(0,len(requestList)):
+            if (100+(i*30)) > 450:
+                break
+            singer,song=requestList[i].split(":")
+            songname=song.split(" [")[0]
+            artist, title = songname.split(" - ", 1)
+            #retString = "%s\n%s - %s" %(retString, singer, title)
+            retString = "%s\n%s" %(retString, song)
+        return retString
+
+    def playSong(self, requestList):
+        songname=requestList[0]
+        html_parser=HTMLParser.HTMLParser()
+        requestList[0]=html_parser.unescape(requestList[0])
+        type=requestList[0].split(".")[1]
+        if type == "cdg":
+            singer,song=requestList[0].split(":")
+            print 'Playing CDG %s(%s)' % (singer, song)
+            #playRequestedCDG(singer, song)
+            os.remove('%s/%s' % (path, songname))
+        elif type == "mp3":
+            print 'Playing MP3 %s' % (requestList[0])
+            #playRequestedMP3(requestList[0])
+            os.remove('%s/%s' % (path, songname))
+        elif type == "m3u":
+            print 'Playing Songlist %s' % (requestList[0])
+            #playRequested3U(dirlist[0])
+            os.remove('%s/%s' % (path, songname))
+
+    def getDirList(self):
+        try:
+            dirlist = os.listdir(path)
+            dirlist.sort(key=lambda x: os.path.getmtime(os.path.join(path, x)))
+        except Exception, err:
+            print 'ERROR: %s, %s' % (str(err), str(Exception))
+        return dirlist
+
+    def sleep(self, seconds):
+        count = 0
+        while count < seconds * 60:
+            count += 1
+            self.clock.tick(60)
+            for event in pygame.event.get():
+                self.checkQuit(event)
+            self.screen.blit(self.background, (0, 0))
+            pygame.display.flip()
+        return
+        
+    def checkQuit(self, event):
+        if event.type == QUIT:
+            sys.exit(0)
+        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+            sys.exit(0)
 
     def resetBackground(self):
         self.background.fill((250, 250, 250))
@@ -267,26 +369,15 @@ class GatorokeServer():
             self.resetBackground()
         text = self.font.render(message, True, (10, 10, 10))
         textrect = text.get_rect()
-        textrect.centerx = self.background.get_width()/2
+        #textrect.centerx = self.background.get_width()/2
         textrect.centery = self.background.get_height()/2
         for line in message.split('\n'):
-            print line
             text = self.font.render(line, True, (10, 10, 10))
-            # by changing the y coordinate each i from lines will appear just
-            # below the previous i
-            #textpos = text.get_rect(centerx=self.background.get_width()/2, centery=self.background.get_height()/2)
-            textrect.centery += 50
+            textrect.centery += 30
             self.background.blit(text, textrect)
 
-    def waitScreen():
-        writeMessage("Waiting for Requests...", clearbg=True)
-        fontsize=36
-        image = Image.new("RGBA", (640,480), (255,255,255))
-        draw = ImageDraw.Draw(image)
-        font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSansBold.ttf", fontsize)
-        draw.text((10, 10), "Waiting for Requests...", (0,0,0), font=font)
-        draw.text((10, 100), "Go to http://karaoke.thehern.org",  (0,0,0), font=font)
-        draw.text((10, 200), "to put in a request!!!", (0,0,0), font=font)
+    def waitScreen(self):
+        self.writeMessage("Waiting for Requests...", clearbg=True)
 
 def main():
     myServer = GatorokeServer()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Gatoroke
 Gatoroake python pygame based karaoke server
+
+
+Requires:
+sudo apt-get install apache2 python-pygame python-imaging python-pykaraoke
